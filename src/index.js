727ad8d3c8047e6d8d750fac472504dd30e8dda8
otakustay <otakustay@gmail.com>
2018-10-11 18:57:41 +0800

    Duplicate declration of react

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+function Button() {
+    return React.createElement(
+        'div',
+        null
+    );
+}
+
+Button.propTypes = {
+    type: PropTypes.string,
+};
